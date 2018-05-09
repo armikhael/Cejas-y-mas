349539634f1c0dcf4b0d78db3b405e8e5a2f4184
Carlos Espinoza <carlosarmikhael@gmail.com>
2018-05-09 02:19:48 -0400

    efectos

--- vendor/parallax/index.js
+++ vendor/parallax/index.js
@@ -4,3 +4,15 @@
 	scalarX: 10,
 	 frictionY: .1
 });
+$('#parallax2').parallax({
+	invertX: true,
+	invertY: true,
+	scalarX: 10,
+	 frictionY: .1
+});
+$('#parallax3').parallax({
+	invertX: true,
+	invertY: true,
+	scalarX: 10,
+	 frictionY: .1
+});
